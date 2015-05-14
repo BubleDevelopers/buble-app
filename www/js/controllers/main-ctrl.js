0659a7085dac6666e383c37333fdccc82f45fd51
Max Dumas <maltor124@gmail.com>
2015-05-14 18:58:17 -0400

    Add address lookup, place finding

--- www/js/controllers/main-ctrl.js
+++ www/js/controllers/main-ctrl.js
@@ -3,7 +3,7 @@
 
 	angular
 	.module('app')
-	.controller('MainCtrl', function($scope, cordovaReady, geolocation) {
+	.controller('MainCtrl', function($scope, $http, cordovaReady, geolocation) {
 		// cordovaReady(function(success, error, options) {
 		// 	if(success) { console.log('Successfully initialized'); }
 		// 	else {
@@ -16,10 +16,46 @@
 			geolocation.getCurrentPosition()
 				.then(function(pos) {
 					$scope.location = pos;
+					$scope.getAddress(pos);
+					$scope.findPlaces(pos);
 				})
 				.catch(function(err) {
 					console.log(err);
 				});
+		};
+
+		$scope.getAddress = function(location) {
+			var params = {
+				'lat': location.coords.latitude,
+				'long': location.coords.longitude
+			};
+			$http({
+				url: 'http://localhost:3001/address',
+				params: params,
+				method: 'GET'
+			}).then(function(res) {
+				location.address = res.data.results[0];
+				console.log(res);
+			});
+		};
+
+		$scope.findPlaces = function(location) {
+			var params = {
+				'lat': location.coords.latitude,
+				'long': location.coords.longitude
+			};
+			$http({
+				url: 'http://localhost:3001/locations',
+				params: params,
+				method: 'GET'
+			}).then(function(res) {
+				$scope.places = res.data.results;
+				console.log(res);
+			});
+		};
+
+		$scope.setPlace = function(place) {
+			console.log(place);
 		};
 	});
 })();
