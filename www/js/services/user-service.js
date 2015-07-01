56f5a75e49ad07ff65ed876e5a87a8285c0b879a
Max Dumas <maltor124@gmail.com>
2015-07-01 00:07:12 -0400

    Add User Service

---
+++ www/js/services/user-service.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+	angular
+	.module('app')
+	.factory('userService', function($http) {
+		var HOST = 'http://localhost:3001';
+		var URL_ROOT = HOST + '/users';
+
+		return {
+			getUserById: function(userId) {
+				return $http({
+					url: URL_ROOT + '/' + userId,
+					method: 'GET'
+				});
+			}
+		};
+	});
+})();
